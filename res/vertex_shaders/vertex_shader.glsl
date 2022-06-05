a2edf06bc776daf3f96b87dfe6a49777654ac0bc
rhuibertsjr <rhuibertsjr@gmail.com>
2022-06-05 18:05:30 +0200

    Added: Normalized device coordinate transformations

--- res/vertex_shaders/vertex_shader.glsl
+++ res/vertex_shaders/vertex_shader.glsl
@@ -1,17 +1,17 @@
 #version 330 core
 
 layout (location = 0) in vec3 aPos;   
-layout (location = 1) in vec3 aColour; 
-layout (location = 2) in vec2 aTextureCoords; 
+layout (location = 1) in vec2 aTextureCoords; 
   
-out vec3 colour; 
 out vec2 texture_coords;
 
-uniform mat4 transform;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main()
 {
-    gl_Position = transform * vec4(aPos, 1.0f);
+    /* Matrix reads right to left. */    
+    gl_Position = projection * view * model * vec4(aPos, 1.0); 
     texture_coords = vec2(aTextureCoords.x, aTextureCoords.y);
-    colour = aColour; 
 }    
--- src/main.c
+++ src/main.c
@@ -8,6 +8,50 @@
 
 #include <cglm/cglm.h>
 
+float vertices[] = {
+    -0.3f, -0.3f, -0.3f,  0.0f, 0.0f,
+     0.3f, -0.3f, -0.3f,  1.0f, 0.0f,
+     0.3f,  0.3f, -0.3f,  1.0f, 1.0f,
+     0.3f,  0.3f, -0.3f,  1.0f, 1.0f,
+    -0.3f,  0.3f, -0.3f,  0.0f, 1.0f,
+    -0.3f, -0.3f, -0.3f,  0.0f, 0.0f,
+
+    -0.3f, -0.3f,  0.3f,  0.0f, 0.0f,
+     0.3f, -0.3f,  0.3f,  1.0f, 0.0f,
+     0.3f,  0.3f,  0.3f,  1.0f, 1.0f,
+     0.3f,  0.3f,  0.3f,  1.0f, 1.0f,
+    -0.3f,  0.3f,  0.3f,  0.0f, 1.0f,
+    -0.3f, -0.3f,  0.3f,  0.0f, 0.0f,
+
+    -0.3f,  0.3f,  0.3f,  1.0f, 0.0f,
+    -0.3f,  0.3f, -0.3f,  1.0f, 1.0f,
+    -0.3f, -0.3f, -0.3f,  0.0f, 1.0f,
+    -0.3f, -0.3f, -0.3f,  0.0f, 1.0f,
+    -0.3f, -0.3f,  0.3f,  0.0f, 0.0f,
+    -0.3f,  0.3f,  0.3f,  1.0f, 0.0f,
+
+     0.3f,  0.3f,  0.3f,  1.0f, 0.0f,
+     0.3f,  0.3f, -0.3f,  1.0f, 1.0f,
+     0.3f, -0.3f, -0.3f,  0.0f, 1.0f,
+     0.3f, -0.3f, -0.3f,  0.0f, 1.0f,
+     0.3f, -0.3f,  0.3f,  0.0f, 0.0f,
+     0.3f,  0.3f,  0.3f,  1.0f, 0.0f,
+
+    -0.3f, -0.3f, -0.3f,  0.0f, 1.0f,
+     0.3f, -0.3f, -0.3f,  1.0f, 1.0f,
+     0.3f, -0.3f,  0.3f,  1.0f, 0.0f,
+     0.3f, -0.3f,  0.3f,  1.0f, 0.0f,
+    -0.3f, -0.3f,  0.3f,  0.0f, 0.0f,
+    -0.3f, -0.3f, -0.3f,  0.0f, 1.0f,
+    
+    -0.3f,  0.3f, -0.3f,  0.0f, 1.0f,
+     0.3f,  0.3f, -0.3f,  1.0f, 1.0f,
+     0.3f,  0.3f,  0.3f,  1.0f, 0.0f,
+     0.3f,  0.3f,  0.3f,  1.0f, 0.0f,
+    -0.3f,  0.3f,  0.3f,  0.0f, 0.0f,
+    -0.3f,  0.3f, -0.3f,  0.0f, 1.0f
+};
+
 int main(int argc, char** argv)
 {
     /* Initalization */
@@ -32,53 +76,33 @@ int main(int argc, char** argv)
     glewExperimental = GL_TRUE;
     glewInit();
 
+    /* OpenGL functionality */
+    glEnable(GL_DEPTH_TEST);
+
     /* Shaders */
     unsigned int shader_program;
     shader_program = gogh_shader_create(
         "./res/vertex_shaders/vertex_shader.glsl",
         "./res/fragment_shaders/fragment_shader.glsl");
     
-    // Adding the vertex data
-    float vertices[] = {
-        // positions          // colors           // texture coords
-         0.5f,  0.7f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-         0.5f, -0.7f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-        -0.5f, -0.7f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-        -0.5f,  0.7f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
-    };
-
-    unsigned int indices[] = {  
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
-    };
-    
-    unsigned int vbo, vao, ebo;
+    unsigned int vbo, vao;
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
-    glGenBuffers(1, &ebo);
 
     glBindVertexArray(vao);
     
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
     // Position attribute
     glVertexAttribPointer(
-        0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+        0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) 0);
     glEnableVertexAttribArray(0);
-
-    // Colour attribute
-    glVertexAttribPointer(
-        1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3* sizeof(float)));
-    glEnableVertexAttribArray(1);
 
     // Texture attribute
     glVertexAttribPointer(
-        2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6* sizeof(float)));
-    glEnableVertexAttribArray(2);
+        1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) (3* sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     /* Textures */
     int width, height, channels;
@@ -103,37 +127,68 @@ int main(int argc, char** argv)
     glGenerateMipmap(GL_TEXTURE_2D);
 
     stbi_image_free(data);
+
+    vec3 cube_positions[10] = {
+        {  0.0f,  0.0f, -6.0f  },
+        {  0.6f,  3.2f, -7.0f },
+        { -1.2f, -1.2f, -2.5f  },
+        { -2.3f, -0.7f, -8.3f },
+        {  0.4f, -1.4f, -3.5f  },
+        { -1.0f,  2.0f, -4.5f  },
+        {  1.3f, -2.0f, -2.5f  },
+        {  0.4f,  1.0f, -2.5f  },
+        {  1.5f,  0.4f, -1.5f  },
+        { -1.3f,  1.0f, -1.5f }
+    };
         
     /* Rendering */
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(GOGH_COLOUR(242), GOGH_COLOUR(242), GOGH_COLOUR(247), 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        /* Triangle */
+        /* Textures */
         glBindTexture(GL_TEXTURE_2D, texture);
+
+        /* Shaders */
         gogh_shader_bind(&shader_program);
-        glBindVertexArray(vao);
 
-        /* Transformation */
-        mat4 trans = {{1.0f, 0.0f, 0.0f, 0.0f},
-                      {0.0f, 1.0f, 0.0f, 0.0f},
-                      {0.0f, 0.0f, 1.0f, 0.0f},
-                      {0.0f, 0.0f, 0.0f, 1.0f}};
-
-        glm_translate_to(trans, (vec3){ 0.5f, -0.5f, 0.0f}, trans);
-        glm_rotate(trans, (float)glfwGetTime(), (vec3){0.0f, 0.0f, 1.0f});
-
-        unsigned int location = glGetUniformLocation(shader_program, "transform");
-        glUniformMatrix4fv(location, 1, GL_FALSE, (float*) trans);
+        /* Transformations */
+        mat4 view;
+        glm_mat4_identity(view);
         
+        glm_translate_to(view, (vec3){ 0.0f, 0.0f, -3.0f}, view);
+
+        mat4 projection;
+        glm_mat4_identity(projection);
+
+        glm_perspective(glm_rad(45.0f), 1280 / 720, 0.1f, 100.0f, projection);
+        
+        unsigned int view_location = glGetUniformLocation(shader_program, "view");
+        glUniformMatrix4fv(view_location, 1, GL_FALSE, (float*) view);
+
+        unsigned int projection_location = glGetUniformLocation(shader_program, "projection");
+        glUniformMatrix4fv(projection_location, 1, GL_FALSE, (float*) projection);
+                
         // Wireframe mode
         //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
         // Use if using EBO
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
-        
+        //glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+        for (int i = 0; i < 10; i++) {
+            mat4 model;
+            glm_mat4_identity(model);
+            glm_translate(model, cube_positions[i]);
+
+            float angle = 20.0f * (i + 0.5);
+            glm_rotate(model, (float) glfwGetTime() * glm_rad(angle), (vec3) { 1.0f, 0.3f, 0.5f });
+            unsigned int model_location = glGetUniformLocation(shader_program, "model");
+            glUniformMatrix4fv(model_location, 1, GL_FALSE, (float*) model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+                
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -141,7 +196,6 @@ int main(int argc, char** argv)
     /* Freeing */
     glDeleteVertexArrays(1, &vao);
     glDeleteBuffers(1, &vbo);
-    glDeleteBuffers(1, &ebo);
 
     /* Closing */
     glfwTerminate();
